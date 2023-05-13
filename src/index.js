92c57c7e7e9be72894c6035cd582c925be1f6804
ThePythonGuyy <thepythonguy@outlook.com>
2023-05-13 17:02:27 +0530

    OrderBy and Timestamps

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app";
 import {
   collection,
   getFirestore,
@@ -8,81 +8,80 @@ import {
   doc,
   onSnapshot,
   query,
-  where
-} from 'firebase/firestore'
+  where,
+  orderBy,
+  serverTimestamp,
+} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBymh5Kh6a-t8GObVVwyuJ6h7dBYKEqo5M",
-    authDomain: "fir-startup-e554f.firebaseapp.com",
-    projectId: "fir-startup-e554f",
-    storageBucket: "fir-startup-e554f.appspot.com",
-    messagingSenderId: "136552291737",
-    appId: "1:136552291737:web:4f54efd44eaecb2080c708"
-  };
+  apiKey: "AIzaSyBymh5Kh6a-t8GObVVwyuJ6h7dBYKEqo5M",
+  authDomain: "fir-startup-e554f.firebaseapp.com",
+  projectId: "fir-startup-e554f",
+  storageBucket: "fir-startup-e554f.appspot.com",
+  messagingSenderId: "136552291737",
+  appId: "1:136552291737:web:4f54efd44eaecb2080c708",
+};
 
-  // init firebase app
-initializeApp(firebaseConfig)
+// init firebase app
+initializeApp(firebaseConfig);
 
-  //init services
-const db = getFirestore()
+//init services
+const db = getFirestore();
 
-  // collection ref
-const colRef = collection(db,'Books')
+// collection ref
+const colRef = collection(db, "Books");
 
-  // get collection data
+// get collection data
 getDocs(colRef)
-    .then((snapshot) => {
-      let books = []
-      snapshot.docs.forEach((doc)=>{
-        books.push({ ...doc.data(), id: doc.id })
-      })
-      console.log(books)
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
+  .then((snapshot) => {
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(books);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
-  
-
-  
 // adding documents
-const addBookForm = document.querySelector('.add')
-addBookForm.addEventListener('submit', (e) => {
-  e.preventDefault()
+const addBookForm = document.querySelector(".add");
+addBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
   addDoc(colRef, {
     title: addBookForm.title.value,
     author: addBookForm.author.value,
-  })
-  .then(() => {
-    addBookForm.reset()
-  })
-})
-
-
+    createdAt: serverTimestamp()
+  }).then(() => {
+    addBookForm.reset();
+  });
+});
 
 // deleting documents
-const deleteBookForm = document.querySelector('.delete')
-deleteBookForm.addEventListener('submit', (e) => {
-  e.preventDefault()
+const deleteBookForm = document.querySelector(".delete");
+deleteBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-  const docRef = doc(db, 'Books', deleteBookForm.id.value)
-  deleteDoc(docRef)
-    .then(() => {
-      deleteBookForm.reset()
-    })
-
-})
+  const docRef = doc(db, "Books", deleteBookForm.id.value);
+  deleteDoc(docRef).then(() => {
+    deleteBookForm.reset();
+  });
+});
 
 //Queries
-const q = query(colRef, where('author', '==', 'Harry'))
+const q = query(
+  colRef,
+  where("author", "==", "Harry"),
+  orderBy("createdAt"),
+
+);
 
 // Real time database
-onSnapshot(q,(snap) => {
+onSnapshot(q, (snap) => {
   let books = [];
   snap.docs.forEach((doc) => {
-    books.push({...doc.data(), id: doc.id })
-    
-  })
-  console.log(books)
-})
+    books.push({ ...doc.data(), id: doc.id });
+  });
+  console.log(books);
+});

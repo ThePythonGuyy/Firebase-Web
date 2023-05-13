fd0e95a10b1294874cc9fe47d50051de7238bef2
ThePythonGuyy <thepythonguy@outlook.com>
2023-05-13 14:12:45 +0530

    Setted up add and delete document

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import {
   collection,
   getFirestore,
   getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -15,16 +18,16 @@ const firebaseConfig = {
   };
 
   // init firebase app
-  initializeApp(firebaseConfig)
+initializeApp(firebaseConfig)
 
   //init services
-  const db = getFirestore()
+const db = getFirestore()
 
   // collection ref
-  const colRef = collection(db,'Books')
+const colRef = collection(db,'Books')
 
   // get collection data
-  getDocs(colRef)
+getDocs(colRef)
     .then((snapshot) => {
       let books = []
       snapshot.docs.forEach((doc)=>{
@@ -35,4 +38,34 @@ const firebaseConfig = {
     .catch(err => {
       console.log(err.message)
     })
+
+  
+// adding documents
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+  })
+  .then(() => {
+    addBookForm.reset()
+  })
+})
+
+
+
+// deleting documents
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const docRef = doc(db, 'Books', deleteBookForm.id.value)
+  deleteDoc(docRef)
+    .then(() => {
+      deleteBookForm.reset()
+    })
+
+})
 
